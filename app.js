2f37e216933e8756ea4009a0388d2d1df51c0a11
Dylan Taft <dylbronjames@gmail.com>
2020-04-16 20:24:20 -0600

    database added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -13,6 +14,30 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//mongodb connection
+mongoose.connect("mongodb://localhost:27017/profilerDB", { useNewUrlParser: true });
+mongoose.set("useCreateIndex", true);
+//create schema
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 app.get("/", function (req, res) {
   res.render("home");
